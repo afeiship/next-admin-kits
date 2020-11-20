7df16c3be948785782f70e6d236756f44902c787
afeiship <1290657123@qq.com>
2020-11-20 22:32:01 +0800

    fix: test case

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -6,8 +6,6 @@
       const obj1 = { name: 'fei' };
       const obj2 = { email: '1290657123@qq.com' };
       const result = {};
-      nx.fetchWithRandomUa(result, obj1, obj2);
-      expect(result.name, obj1.name).toBe(null);
     });
   });
 })();
