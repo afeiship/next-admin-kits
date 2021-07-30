f3d80a3235630516b5070dc23d91b16fbbf2f4b8
afeiship <1290657123@qq.com>
2021-07-30 09:30:27 +0800

    fix: rua every time

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
   var nx = global.nx || require('@jswork/next');
   var nxDeepAssign = nx.deepAssign || require('@jswork/next-deep-assign');
   var nxRandomUa = nx.randomUa || require('@jswork/next-random-ua');
-  var DEFAULT_OPTIONS = {
-    headers: { 'User-Agent': nxRandomUa() }
-  };
+  var defaults = { rua: false };
 
   nx.fetchWithRandomUa = function (inFetch) {
     return function (inUrl, inOptions) {
-      var options = nxDeepAssign(null, DEFAULT_OPTIONS, inOptions);
+      var options = nx.mix(null, defaults, inOptions);
+      var randomUa = { headers: { 'User-Agent': nxRandomUa() } };
+      options.rua && (options = nxDeepAssign(randomUa, options));
       return inFetch(inUrl, options);
     };
   };
