7d16bded262e55800d99bfaabf8d3895cf482fc1
afeiship <1290657123@qq.com>
2023-04-22 10:10:10 +0800

    chore: add deep equal

--- src/index.js
+++ src/index.js
@@ -5,23 +5,24 @@ import EventMitt from '@jswork/event-mitt';
 import '@jswork/next-local-storage';
 
 // packages
-import "@jswork/next-ant-column"
-import "@jswork/next-capitalize"
-import "@jswork/next-classify"
-import "@jswork/next-compact-object"
-import "@jswork/next-deep-assign"
-import "@jswork/next-deep-clone"
-import "@jswork/next-deep-each"
-import "@jswork/next-empty"
-import "@jswork/next-get2get"
-import "@jswork/next-is-empty-object"
-import "@jswork/next-is-plain-object"
-import "@jswork/next-kebab-case"
-import "@jswork/next-key-map"
-import "@jswork/next-param"
-import "@jswork/next-qs"
-import "@jswork/next-sets"
-import "@jswork/next-json"
+import '@jswork/next-ant-column';
+import '@jswork/next-capitalize';
+import '@jswork/next-classify';
+import '@jswork/next-compact-object';
+import '@jswork/next-deep-assign';
+import '@jswork/next-deep-clone';
+import '@jswork/next-deep-each';
+import '@jswork/next-deep-equal';
+import '@jswork/next-empty';
+import '@jswork/next-get2get';
+import '@jswork/next-is-empty-object';
+import '@jswork/next-is-plain-object';
+import '@jswork/next-kebab-case';
+import '@jswork/next-key-map';
+import '@jswork/next-param';
+import '@jswork/next-qs';
+import '@jswork/next-sets';
+import '@jswork/next-json';
 
 const defaults = { prefix: 'nak' };
 
