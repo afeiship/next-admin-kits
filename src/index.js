5306c4cd6746b94dcb9ac704e8f84588a08b3b89
afeiship <1290657123@qq.com>
2024-07-16 20:51:31 +0800

    feat: add nx-guid

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ import '@jswork/next-qs';
 import '@jswork/next-sets';
 import '@jswork/next-json';
 import '@jswork/next-to-clipboard';
+import '@jswork/next-guid';
 
 const defaults = { prefix: 'nak' };
 
